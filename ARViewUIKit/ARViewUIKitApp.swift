5a9407a2ee0e47b07db6210a6ef7232e22e0a2f0
AdnannM <adnannmuratovic@icloud.com>
2025-04-12 09:34:06 +0200

    Initial Commit

---
+++ ARViewUIKit/ARViewUIKitApp.swift
@@ -0,0 +1,17 @@
+//
+//  ARViewUIKitApp.swift
+//  ARViewUIKit
+//
+//  Created by Adnann Muratovic on 12.04.25.
+//
+
+import SwiftUI
+
+@main
+struct ARViewUIKitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ARViewUIKit/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ARViewUIKit
+//
+//  Created by Adnann Muratovic on 12.04.25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
